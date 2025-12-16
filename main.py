cf4dbc29bd243c1fa1e4922aee299ab7710440f3
kdezer <kdezer1992@gmail.com>
2025-12-16 20:28:21 +0100

    add weather notification service

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 from core.config.config_loader import Config
 from core.domains.location import Location
-from infrastructure.messengers.console_messenger import ConsoleMessenger
 from infrastructure.messengers.telegram_messenger import TelegramMessenger
 from infrastructure.weather_api_client import WeatherApiClient
+from service.weather_notification_service import WeatherNotificationService
 from service.weather_service import WeatherService
 
 
 def run():
     config = Config.from_files()
+    location = Location(name=config.name,latitude=config.latitude,longitude=config.longitude)
+    weather_client = WeatherApiClient()
+    weather_service = WeatherService(weather_client)
+    messenger = TelegramMessenger(chat_id=config.telegram_chat_id, api_token=config.telegram_api_token)
+    notification_service = WeatherNotificationService(messenger=messenger,weather_service=weather_service)
+    notification_service.run(location=location)
 
-    location = Location(
-        name=config.name,
-        latitude=config.latitude,
-        longitude=config.longitude
-    )
-    weather_api_client = WeatherApiClient()
-    weather_service = WeatherService(weather_api_client=weather_api_client)
-    console_messenger = ConsoleMessenger()
-    console_messenger.send_message(weather_service.get_weather_summary(location=location))
-    telegram_messenger = TelegramMessenger(config.telegram_chat_id,config.telegram_api_token)
-    telegram_messenger.send_message(weather_service.get_weather_summary(location=location))
 if __name__ == "__main__":
     run()
---
+++ service/weather_notification_service.py
@@ -0,0 +1,14 @@
+from core.domains.location import Location
+from core.protocols.protocols import MessengerProtocol
+from service.weather_service import WeatherService
+
+
+class WeatherNotificationService:
+
+    def __init__(self, messenger: MessengerProtocol, weather_service: WeatherService):
+        self.messenger: MessengerProtocol = messenger
+        self.weather_service: WeatherService = weather_service
+
+    def run(self,location:Location):
+        summary = self.weather_service.get_weather_summary(location=location)
+        self.messenger.send_message(summary)
