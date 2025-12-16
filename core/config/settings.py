d9cad4c141ec3959cd0519cf6a9c399e40eb9e7a
kdezer <kdezer1992@gmail.com>
2025-12-16 21:59:31 +0100

    Add retries to weather api client

--- core/config/settings.py
+++ core/config/settings.py
@@ -1,11 +1,11 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    location_name: str
-    latitude: float
-    longitude: float
-    telegram_chat_id: str
-    telegram_api_token: str
+    location_name: str = ""
+    latitude: float = 0
+    longitude: float = 0
+    telegram_chat_id: str = ""
+    telegram_api_token: str = ""
 
     class Config:
         env_file = ".env"
--- infrastructure/weather_api_client.py
+++ infrastructure/weather_api_client.py
@@ -1,5 +1,5 @@
-import logging
 from datetime import datetime
+from time import sleep
 
 import requests  # type: ignore[import-untyped]
 
@@ -14,6 +14,8 @@ class WeatherApiClient(WeatherProtocol):
 
     def get_weather(self, location: Location) -> Weather:
 
+        MAX_ATTEMPT:int = 3
+        WAIT_TIME:float = 5
 
         logger.info(f"Fetching weather for {location}")
 
@@ -26,26 +28,32 @@ class WeatherApiClient(WeatherProtocol):
             "forecast_days": 1,
         }
 
-        try:
-            response = requests.get(url=BASE_URL, params=params, timeout=5)
-            data: dict[str, dict[str, list]] = response.json()
-            logger.info(f"Weather fetched successfully")
+        for attempt in range(MAX_ATTEMPT):
+            try:
+                response = requests.get(url=BASE_URL, params=params, timeout=5)
+                data: dict[str, dict[str, list]] = response.json()
+                logger.info(f"Weather fetched successfully")
 
-            current_hour: int = datetime.now().hour
-            hourly: dict[str, list] = data["hourly"]
+                current_hour: int = datetime.now().hour
+                hourly: dict[str, list] = data["hourly"]
 
-            actual_weather_time: list[str] = [time for time in hourly["time"] if
-                                              int(time.split("T")[1].split(":")[0]) == int(current_hour)]
-            time_index: int = hourly["time"].index(actual_weather_time[0])
+                actual_weather_time: list[str] = [time for time in hourly["time"] if int(time.split("T")[1].split(":")[0]) == int(current_hour)]
+                time_index: int = hourly["time"].index(actual_weather_time[0])
 
-            temp: float = hourly["temperature_2m"][time_index]
-            rain: int = hourly["precipitation_probability"][time_index]
+                temp: float = hourly["temperature_2m"][time_index]
+                rain: int = hourly["precipitation_probability"][time_index]
 
-            return Weather(
-                location=location.name,
-                temperature_celsius=temp,
-                rain_chance=rain
-            )
-        except Exception as e:
-            logger.error("Failed to fetch weather",exc_info=True)
-            raise
+                return Weather(
+                    location=location.name,
+                    temperature_celsius=temp,
+                    rain_chance=rain
+                )
+            except requests.exceptions.RequestException:
+                if attempt == 2:
+                    logger.error("Failed to fetch weather",exc_info=True)
+                    raise
+                logger.warning(f"Attempt {attempt + 1} failed. Retrying in {WAIT_TIME} seconds...")
+                sleep(WAIT_TIME)
+        raise RuntimeError("Unreachable: Weather could not be fetched.")
+
+
